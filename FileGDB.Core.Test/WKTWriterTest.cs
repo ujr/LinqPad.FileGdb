c0c191219bbcb384f237d9d43156a0cc9f9661b3
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2024-07-05 18:04:30 +0200

    Design and write WKTWriter and unit tests
    - so we have a way to show the geometries we read
    - Esri polygons are WKT multipolygons (can have multiple
      top-level parts), but Esri does not represent the nesting
      structure as does WKT... for now probably best to just
      show Esri polygons syntactically as WKT polygons

---
+++ FileGDB.Core.Test/WKTWriterTest.cs
@@ -0,0 +1,249 @@
+using System;
+using System.Text;
+using Xunit;
+
+namespace FileGDB.Core.Test;
+
+public class WKTWriterTest
+{
+	[Fact]
+	public void CanWritePoint()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		buffer.Clear();
+		writer.BeginPoint();
+		writer.AddVertex(12.3, 45.6);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("POINT (12.3 45.6)", buffer.ToString());
+
+		buffer.Clear();
+		writer.WritePoint(1.1, 2.2, 3.3, 99.9, 1);
+		writer.Flush();
+		Assert.Equal("POINT ZMID (1.1 2.2 3.3 99.9 1)", buffer.ToString());
+
+		buffer.Clear();
+		writer.WritePoint(1.1, 2.2, 3.3, 99.9);
+		writer.Flush();
+		Assert.Equal("POINT ZM (1.1 2.2 3.3 99.9)", buffer.ToString());
+
+		buffer.Clear();
+		writer.WritePoint(1.1, 2.2);
+		writer.Flush();
+		Assert.Equal("POINT (1.1 2.2)", buffer.ToString());
+
+		buffer.Clear();
+		writer.BeginPoint();
+		writer.NewPart(); // an allowed no-op, even for a point
+		writer.AddVertex(1, 2);
+		Assert.Throws<InvalidOperationException>(() => writer.NewPart());
+		writer.EndShape();
+	}
+
+	[Fact]
+	public void CanWriteEmptyPoint()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		buffer.Clear();
+		writer.BeginPoint();
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("POINT EMPTY", buffer.ToString());
+
+		buffer.Clear();
+		writer.BeginPoint(hasM: true);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("POINT M EMPTY", buffer.ToString());
+	}
+
+	[Fact]
+	public void CanWriteBox()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		buffer.Clear();
+		writer.WriteBox(1, 2, 51, 42);
+		writer.Flush();
+		Assert.Equal("BOX (1 2, 51 42)", buffer.ToString());
+
+		buffer.Clear();
+		writer.WriteBox(1, 2, 51, 52, 400, 432);
+		writer.Flush();
+		Assert.Equal("BOX Z (1 2 400, 51 52 432)", buffer.ToString());
+
+		buffer.Clear();
+		writer.WriteBox(1, 2, 51, 42, idmin: 0, idmax: 3);
+		writer.Flush();
+		Assert.Equal("BOX ID (1 2 0, 51 42 3)", buffer.ToString());
+
+		buffer.Clear();
+		writer.WriteBox(1, 2, 51, 42, 400, 432, 55.25, 94.5, 0, 3);
+		writer.Flush();
+		Assert.Equal("BOX ZMID (1 2 400 55.25 0, 51 42 432 94.5 3)", buffer.ToString());
+	}
+
+	[Fact]
+	public void CanWriteEmptyBox()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		buffer.Clear();
+		writer.WriteBox(double.NaN, double.NaN, double.NaN, double.NaN);
+		writer.Flush();
+		Assert.Equal("BOX EMPTY", buffer.ToString());
+
+		buffer.Clear();
+		writer.WriteBox(double.NaN, double.NaN, double.NaN, double.NaN, mmin: 55.25, mmax: 94.5);
+		writer.Flush();
+		Assert.Equal("BOX M EMPTY", buffer.ToString());
+	}
+
+	[Fact]
+	public void CanWriteMultipoint()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		writer.BeginMultipoint(hasZ: true);
+		writer.AddVertex(1.1, 1.2, 401.5);
+		writer.AddVertex(2.1, 2.2, 402.5);
+		writer.AddVertex(3.1, 3.2); // Z shall be NaN
+		writer.EndShape();
+
+		writer.Flush();
+		Assert.Equal("MULTIPOINT Z ((1.1 1.2 401.5), (2.1 2.2 402.5), (3.1 3.2 NaN))", buffer.ToString());
+
+		buffer.Clear();
+		writer.BeginMultipoint();
+		writer.NewPart(); // optional
+		writer.NewPart(); // idempotent
+		writer.AddVertex(1.1, 1.2);
+		writer.NewPart();
+		writer.AddVertex(2.1, 2.2);
+		writer.NewPart();
+		writer.AddVertex(3.1, 3.2);
+		writer.NewPart(); // empty parts are not emitted
+		writer.NewPart(); // idempotent
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("MULTIPOINT ((1.1 1.2), (2.1 2.2), (3.1 3.2))", buffer.ToString());
+	}
+
+	[Fact]
+	public void CanWriteLineString()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		buffer.Clear();
+		writer.BeginLineString();
+		writer.AddVertex(0, 0);
+		writer.AddVertex(1, 1);
+		writer.AddVertex(1, 2);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("LINESTRING (0 0, 1 1, 1 2)", buffer.ToString());
+
+		buffer.Clear();
+		writer.BeginLineString(hasZ: true, hasID: true);
+		writer.NewPart(); // an allowed no-op
+		writer.AddVertex(0, 0, 0, id: 0);
+		writer.AddVertex(1, 1, 1, id: 1);
+		Assert.Throws<InvalidOperationException>(() => writer.NewPart());
+		writer.AddVertex(1, 2, 3, id: 0);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("LINESTRING ZID (0 0 0, 1 1 1 1, 1 2 3)", buffer.ToString());
+
+		buffer.Clear();
+		writer.BeginLineString(hasZ: true, hasM: true);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("LINESTRING ZM EMPTY", buffer.ToString());
+	}
+
+	[Fact]
+	public void CanWriteMultiLineString()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		buffer.Clear();
+		writer.BeginMultiLineString();
+		writer.AddVertex(0, 0);
+		writer.AddVertex(1, 1);
+		writer.AddVertex(1, 2);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("MULTILINESTRING ((0 0, 1 1, 1 2))", buffer.ToString());
+
+		buffer.Clear();
+		writer.BeginMultiLineString();
+		writer.AddVertex(0, 0);
+		writer.AddVertex(1, 1);
+		writer.AddVertex(1, 2);
+		writer.NewPart();
+		writer.AddVertex(2, 3);
+		writer.AddVertex(3, 2);
+		writer.NewPart(); // a no-op because empty
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("MULTILINESTRING ((0 0, 1 1, 1 2), (2 3, 3 2))", buffer.ToString());
+
+		buffer.Clear();
+		writer.BeginMultiLineString(hasZ: true);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("MULTILINESTRING Z EMPTY", buffer.ToString());
+	}
+
+	[Fact]
+	public void CanWritePolygon()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		buffer.Clear();
+		writer.BeginPolygon();
+		writer.AddVertex(0, 0);
+		writer.AddVertex(4, 0);
+		writer.AddVertex(4, 4);
+		writer.AddVertex(0, 4);
+		writer.AddVertex(0, 0);
+		writer.NewPart(); // for a WKT POLYGON, new part must be a hole
+		writer.AddVertex(1, 1);
+		writer.AddVertex(2, 1);
+		writer.AddVertex(2, 2);
+		writer.AddVertex(1, 2);
+		writer.AddVertex(1, 1);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("POLYGON ((0 0, 4 0, 4 4, 0 4, 0 0), (1 1, 2 1, 2 2, 1 2, 1 1))", buffer.ToString());
+
+		buffer.Clear();
+		writer.BeginPolygon(true, true, true);
+		writer.EndShape();
+		writer.Flush();
+		Assert.Equal("POLYGON ZMID EMPTY", buffer.ToString());
+	}
+
+	[Fact(Skip="not yet implemented")]
+	public void CanWriteMultiPolygon()
+	{
+		var buffer = new StringBuilder();
+		using var writer = new WKTWriter(buffer);
+
+		buffer.Clear();
+		writer.BeginMultiPolygon();
+		// TODO
+		Assert.Equal("MULTIPOLYGON (((0 0, 4 0, 4 4, 0 4, 0 0), (1 1, 2 1, 2 2, 1 2, 1 1)), ((-1 -1, -1 -2, -2 -2, -2 -1, -1 -1)))",
+			buffer.ToString());
+	}
+}
---
+++ FileGDB.Core/WKTWriter.cs
@@ -0,0 +1,434 @@
+using System.Globalization;
+using System.Text;
+
+namespace FileGDB.Core;
+
+public class WKTWriter : IDisposable
+{
+	private readonly TextWriter _writer;
+	private readonly CultureInfo _invariant = CultureInfo.InvariantCulture;
+	private State _state;
+	private int _partIndex;
+	private int _vertexIndex;
+
+	public WKTWriter(TextWriter writer)
+	{
+		_writer = writer ?? throw new ArgumentNullException(nameof(writer));
+		_state = State.Initial;
+		DecimalDigits = -1;
+	}
+
+	public WKTWriter(StringBuilder sb)
+		: this(new StringWriter(sb)) { }
+
+	// TODO public int MaxLineLength { get; set; }
+	public int DecimalDigits { get; set; }
+
+	public bool DefaultHasZ { get; set; }
+	public bool DefaultHasM { get; set; }
+	public bool DefaultHasID { get; set; }
+
+	public bool CurrentHasZ { get; private set; }
+	public bool CurrentHasM { get; private set; }
+	public bool CurrentHasID { get; private set; }
+
+	// Examples (from postgis.net):
+	// POINT(0 0)
+	// POINT Z (0 0 0)
+	// POINT ZM (0 0 0 0)
+	// POINT EMPTY
+	// LINESTRING(0 0,1 1,1 2)
+	// LINESTRING EMPTY
+	// POLYGON((0 0,4 0,4 4,0 4,0 0),(1 1, 2 1, 2 2, 1 2,1 1))
+	// MULTIPOINT((0 0),(1 2))
+	// MULTIPOINT Z ((0 0 0),(1 2 3))
+	// MULTIPOINT EMPTY
+	// MULTILINESTRING((0 0,1 1,1 2),(2 3,3 2,5 4))
+	// MULTIPOLYGON(((0 0,4 0,4 4,0 4,0 0),(1 1,2 1,2 2,1 2,1 1)), ((-1 -1,-1 -2,-2 -2,-2 -1,-1 -1)))
+
+	public void BeginPoint(bool? hasZ = null, bool? hasM = null, bool? hasID = null)
+	{
+		BeginShape(State.Point, hasZ, hasM, hasID);
+	}
+
+	public void BeginMultipoint(bool? hasZ = null, bool? hasM = null, bool? hasID = null)
+	{
+		BeginShape(State.Multipoint, hasZ, hasM, hasID);
+	}
+
+	public void BeginLineString(bool? hasZ = null, bool? hasM = null, bool? hasID = null)
+	{
+		BeginShape(State.LineString, hasZ, hasM, hasID);
+	}
+
+	public void BeginMultiLineString(bool? hasZ = null, bool? hasM = null, bool? hasID = null)
+	{
+		BeginShape(State.MultiLineString, hasZ, hasM, hasID);
+	}
+
+	public void BeginPolygon(bool? hasZ = null, bool? hasM = null, bool? hasID = null)
+	{
+		BeginShape(State.Polygon, hasZ, hasM, hasID);
+	}
+
+	public void BeginMultiPolygon(bool? hasZ = null, bool? hasM = null, bool? hasID = null)
+	{
+		throw new NotImplementedException();
+		BeginShape(State.MultiPolygon, hasZ, hasM, hasID);
+	}
+
+	public void NewPart()
+	{
+		if (_state == State.Initial)
+			throw InvalidOperation("No current shape");
+
+		// NewPart right after BeginShape is a no-op and always allowed:
+		if (_vertexIndex == 0 && _partIndex == 0) return;
+
+		if (!IsMultiPart(_state))
+			throw InvalidOperation("Current shape is not multi-part");
+
+		if (_vertexIndex > 0)
+		{
+			_partIndex += 1;
+		}
+
+		_vertexIndex = 0;
+	}
+
+	public void AddVertex(double x, double y, double z = double.NaN, double m = double.NaN, int? id = null)
+	{
+		if (_state == State.Initial)
+			throw InvalidOperation("No current shape");
+		if (_state == State.Point && _vertexIndex > 0)
+			throw InvalidOperation("A point can only have one vertex");
+
+		if (!CurrentHasZ && !double.IsNaN(z))
+			throw new ArgumentException("Current shape is not Z aware", nameof(z));
+		if (!CurrentHasM && !double.IsNaN(m))
+			throw new ArgumentException("Current shape is not M aware", nameof(m));
+		if (!CurrentHasID && id.HasValue)
+			throw new ArgumentException("Current shape is not ID aware", nameof(id));
+
+		if (_vertexIndex == 0)
+		{
+			if (_partIndex == 0)
+			{
+				_writer.Write(IsMultiPart(_state) ? " ((" : " (");
+			}
+			else
+			{
+				_writer.Write("), (");
+			}
+		}
+		else if (_vertexIndex > 0)
+		{
+			// Multipoint is a special case
+			_writer.Write(_state == State.Multipoint ? "), (" : ", ");
+		}
+		else
+		{
+			throw Bug($"Bug: {nameof(_vertexIndex)} must not be negative");
+		}
+
+		const string sep = " ";
+
+		WriteOrdinate(x);
+		_writer.Write(sep);
+		WriteOrdinate(y);
+
+		if (CurrentHasZ)
+		{
+			_writer.Write(sep);
+			WriteOrdinate(z);
+		}
+
+		if (CurrentHasM)
+		{
+			_writer.Write(sep);
+			WriteOrdinate(m);
+		}
+
+		if (CurrentHasID && id > 0)
+		{
+			_writer.Write(sep);
+			_writer.Write(id.Value);
+		}
+
+		_vertexIndex += 1;
+	}
+
+	public void EndShape()
+	{
+		if (_state == State.Initial)
+			throw InvalidOperation("No current shape");
+
+		if (_vertexIndex == 0 && _partIndex == 0)
+		{
+			_writer.Write(" EMPTY");
+		}
+		else
+		{
+			if (IsMultiPart(_state))
+			{
+				_writer.Write(")");
+			}
+
+			_writer.Write(")");
+		}
+
+		_state = State.Initial;
+		_partIndex = 0;
+		_vertexIndex = 0;
+	}
+
+	public void WritePoint(double x, double y, double z = double.NaN, double m = double.NaN, int? id = null)
+	{
+		bool hasZ = !double.IsNaN(z);
+		bool hasM = !double.IsNaN(m);
+		bool hasID = id.HasValue;
+
+		BeginPoint(hasZ, hasM, hasID);
+		AddVertex(x, y, z, m, id);
+		EndShape();
+	}
+
+	/// <summary>
+	/// Write WKT for a bounding box (aka envelope).
+	/// This is a NON-CONFORMING extension to WKT!
+	/// Syntax: "BOX (xmin ymin, xmax ymax)" or
+	/// "BOX Z (xmin ymin zmin, xmax ymax zmax" or ...
+	/// </summary>
+	public void WriteBox(double xmin, double ymin, double xmax, double ymax,
+		double zmin = double.NaN, double zmax = double.NaN,
+		double mmin = double.NaN, double mmax = double.NaN,
+		int? idmin = null, int? idmax = null)
+	{
+		bool isEmpty = double.IsNaN(xmin) || double.IsNaN(xmax) ||
+		               double.IsNaN(ymin) || double.IsNaN(ymax) ||
+		               xmin > xmax || ymin > ymax;
+
+		bool hasZ = !double.IsNaN(zmin) && !double.IsNaN(zmax);
+		bool hasM = !double.IsNaN(mmin) && !double.IsNaN(mmax);
+		bool hasID = idmin.HasValue && idmax.HasValue;
+
+		BeginShape(State.Box, hasZ, hasM, hasID);
+
+		if (! isEmpty)
+		{
+			const string sep = " ";
+
+			_writer.Write(" (");
+
+			// minima
+
+			WriteOrdinate(xmin);
+			_writer.Write(sep);
+			WriteOrdinate(ymin);
+
+			if (hasZ)
+			{
+				_writer.Write(sep);
+				WriteOrdinate(zmin);
+			}
+
+			if (hasM)
+			{
+				_writer.Write(sep);
+				WriteOrdinate(mmin);
+			}
+
+			if (hasID)
+			{
+				_writer.Write(sep);
+				_writer.Write(idmin);
+			}
+
+			_writer.Write(", ");
+
+			// maxima
+
+			WriteOrdinate(xmax);
+			_writer.Write(sep);
+			WriteOrdinate(ymax);
+
+			if (hasZ)
+			{
+				_writer.Write(sep);
+				WriteOrdinate(zmax);
+			}
+
+			if (hasM)
+			{
+				_writer.Write(sep);
+				WriteOrdinate(mmax);
+			}
+
+			if (hasID)
+			{
+				_writer.Write(sep);
+				_writer.Write(idmax);
+			}
+
+			_vertexIndex = 2; // to control EndShape
+		}
+
+		EndShape();
+	}
+
+	public void Flush()
+	{
+		_writer.Flush();
+	}
+
+	public void Dispose()
+	{
+		_writer.Flush();
+		_writer.Dispose();
+	}
+
+	private void BeginShape(State state, bool? hasZ = null, bool? hasM = null, bool? hasID = null)
+	{
+		if (_state != State.Initial)
+			throw InvalidOperation($"Must call {nameof(EndShape)} before beginning a new shape");
+
+		CurrentHasZ = hasZ ?? DefaultHasZ;
+		CurrentHasM = hasM ?? DefaultHasM;
+		CurrentHasID = hasID ?? DefaultHasID;
+
+		_writer.Write(GetTag(state));
+
+		if (CurrentHasZ || CurrentHasM || CurrentHasID)
+		{
+			_writer.Write(" ");
+			_writer.Write(GetDim(CurrentHasZ, CurrentHasM, CurrentHasID));
+		}
+
+		_state = state;
+		_partIndex = 0;
+		_vertexIndex = 0;
+
+		// NB: cannot write opening paren here as the shape may be empty!
+	}
+
+	private void WriteOrdinate(double value)
+	{
+		if (double.IsNaN(value))
+		{
+			_writer.Write("NaN");
+		}
+		else
+		{
+			var format = GetOrdinateFormatString(DecimalDigits);
+			var text = value.ToString(format, _invariant);
+			_writer.Write(text);
+		}
+	}
+
+	private static string GetOrdinateFormatString(int decimalDigits)
+	{
+		switch (decimalDigits)
+		{
+			case 0: return "F0";
+			case 1: return "F1";
+			case 2: return "F2";
+			case 3: return "F3";
+			case 4: return "F4";
+			case 5: return "F5";
+			case 6: return "F6";
+			case 7: return "F7";
+			case 8: return "F8";
+			case 9: return "F9";
+			case 10: return "F10";
+			case 11: return "F11";
+			case 12: return "F12";
+			case 13: return "F13";
+			case 14: return "F14";
+			case 15: return "F15";
+		}
+
+		return "G";
+	}
+
+	private static bool IsMultiPart(State state)
+	{
+		switch (state)
+		{
+			case State.Multipoint:
+			case State.MultiLineString:
+			case State.MultiPolygon:
+				return true;
+			case State.Polygon:
+				return true; // we consider holes to be parts
+			default:
+				return false;
+		}
+	}
+
+	private static string GetTag(State state)
+	{
+		switch (state)
+		{
+			case State.Point:
+				return "POINT";
+			case State.Multipoint:
+				return "MULTIPOINT";
+			case State.LineString:
+				return "LINESTRING";
+			case State.MultiLineString:
+				return "MULTILINESTRING";
+			case State.Polygon:
+				return "POLYGON";
+			case State.MultiPolygon:
+				return "MULTIPOLYGON";
+			case State.Box:
+				return "BOX";
+		}
+
+		throw new ArgumentOutOfRangeException(nameof(state), state, "No WKT tag for this state");
+	}
+
+	private static string GetDim(bool hasZ, bool hasM, bool hasID)
+	{
+		int key = 0;
+
+		if (hasZ) key += 1;
+		if (hasM) key += 2;
+		if (hasID) key += 4;
+
+		switch (key)
+		{
+			case 1:
+				return "Z";
+			case 2:
+				return "M";
+			case 3:
+				return "ZM";
+			case 4:
+				return "ID";
+			case 5:
+				return "ZID";
+			case 6:
+				return "MID";
+			case 7:
+				return "ZMID";
+		}
+
+		return string.Empty;
+	}
+
+	private InvalidOperationException InvalidOperation(string message)
+	{
+		return new InvalidOperationException(message);
+	}
+
+	private Exception Bug(string message)
+	{
+		return new InvalidOperationException(message ?? "Bug");
+	}
+
+	private enum State
+	{
+		Initial, Point, Multipoint, LineString, MultiLineString, Polygon, MultiPolygon, Box
+	}
+}
