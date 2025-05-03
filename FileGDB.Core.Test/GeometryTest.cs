38d1e1d50613cb258548f42fae9a287cf2e4ac0a
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2025-05-03 21:21:18 +0200

    Some geometry computation: circumcircle, central angle

--- FileGDB.Core.Test/GeometryTest.cs
+++ FileGDB.Core.Test/GeometryTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace FileGDB.Core.Test;
@@ -29,5 +30,65 @@ public class GeometryTest
 		Assert.False(env.IsEmpty);
 		Assert.True(env.Contains(2, -1));
 		Assert.False(env.Contains(2, -1.0000001));
+	}
+
+	[Fact]
+	public void CanSignedAndHeronArea()
+	{
+		var a = new XY(1, 1);
+		var b = new XY(2, 4);
+		var c = new XY(5, 3);
+
+		const int precision = 7; // decimal places
+
+		Assert.Equal(5.0, Geometry.HeronArea(a, b, c), precision);
+		Assert.Equal(5.0, Geometry.HeronArea(c, b, a), precision);
+
+		Assert.Equal(-5.0, Geometry.SignedArea(a, b, c), precision);
+		Assert.Equal(5.0, Geometry.SignedArea(c, b, a), precision);
+	}
+
+	[Fact]
+	public void CanCircumcircle()
+	{
+		// Data and expected results from
+		// https://math.stackexchange.com/questions/213658/get-the-equation-of-a-circle-when-given-3-points
+
+		var a = new XY(1, 1);
+		var b = new XY(2, 4);
+		var c = new XY(5, 3);
+		// Circumference: radius=sqrt(5), center=(3,2)
+
+		const int precision = 7; // decimal places
+
+		Assert.Equal(Math.Sqrt(5), Geometry.CircumcircleRadius(a, b, c), precision);
+
+		var r = Geometry.Circumcircle(a, b, c, out var center);
+		Assert.Equal(Math.Sqrt(5), r, precision);
+		Assert.Equal(3.0, center.X, precision);
+		Assert.Equal(2.0, center.Y, precision);
+
+		// TODO much more: different quadrants, degenerate cases
+	}
+
+	[Fact]
+	public void CanCentralAngle()
+	{
+		var a = new XY(2, 1);
+		var b = new XY(1, 2);
+		var o = new XY(0, 0);
+
+		const int precision = 4; // decimal places
+
+		Assert.Equal(0.6435, Geometry.CentralAngle(a, o, b), precision);
+		Assert.Equal(5.6397, Geometry.CentralAngle(a, o, b, true), precision);
+		Assert.Equal(5.6397, Geometry.CentralAngle(b, o, a), precision);
+		Assert.Equal(0.6435, Geometry.CentralAngle(b, o, a, true), precision);
+
+
+		Assert.Equal(0.0, Geometry.CentralAngle(a, o, a), 9);
+		Assert.Equal(2 * Math.PI, Geometry.CentralAngle(a, o, a, true), 9);
+
+		// TODO much more: different quadrants, degenerate cases
 	}
 }
