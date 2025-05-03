e14f1f5f3af336826fcb9350fbd18fbde6932621
Urs-Jakob Ruetschi <u.rueetschi@esri.ch>
2025-05-03 21:10:24 +0200

    Enhance Envelope: Contains(), Expand(), IsEmpty

--- FileGDB.Core.Test/GeometryTest.cs
+++ FileGDB.Core.Test/GeometryTest.cs
@@ -10,4 +10,24 @@ public class GeometryTest
 		Assert.True(XY.Empty.IsEmpty);
 		Assert.True(double.IsNaN(XY.Empty.Magnitude));
 	}
+
+	[Fact]
+	public void CanEnvelope()
+	{
+		var env = new Envelope();
+
+		Assert.True(env.IsEmpty);
+
+		Assert.False(env.Contains(0, 0));
+		Assert.False(env.Contains(double.NaN, double.NaN));
+
+		env.Expand(0, 0);
+		Assert.False(env.IsEmpty);
+		Assert.True(env.Contains(0, 0));
+
+		env.Expand(2, -1);
+		Assert.False(env.IsEmpty);
+		Assert.True(env.Contains(2, -1));
+		Assert.False(env.Contains(2, -1.0000001));
+	}
 }
